7bb51c2e6ae3c3883cc6d31b75076e567d2310a6
Joseph R Miles <joe@jrm.dev>
2023-03-21 07:39:16 -0700

    chore: wrote first test!

---
+++ alleygator/__init__.py
@@ -0,0 +1,1 @@
+from .alleygator import sum
--- test/test.py
+++ test/test.py
@@ -1,1 +1,13 @@
 import unittest
+
+from alleygator import sum
+
+class TestSum(unittest.TestCase):
+    def test_sum_adds_to_numbers(self):
+        """
+        Sum should be able to just add two numbers.
+        """
+        self.assertEqual(sum(1, 1), 2)
+
+if __name__ == "__main__":
+    unittest.main()
